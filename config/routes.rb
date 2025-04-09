5150bc52b1b45456f5e8ccdcaa09d43ebccf7735
Francois Dubois <francois.dubois1994@gmail.com>
2025-04-09 09:48:50 +0200

    architecture done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'itineraries/index'
-  get 'itineraries/show'
-  get 'itineraries/new'
-  get 'itineraries/create'
-  get 'itineraries/edit'
-  get 'itineraries/update'
-  get 'itineraries/destroy'
-  get 'reviews/create'
-  get 'attractions/index'
-  get 'attractions/show'
-  get 'destinations/index'
-  get 'destinations/show'
-  get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
@@ -28,10 +15,8 @@ Rails.application.routes.draw do
   resources :itineraries do
     resources :itinerary_items, only: [:create, :update, :destroy]
   end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
